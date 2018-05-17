d524c413218eaf649564c9047252522f8c1cbe34
Moses <ysdxz207@qq.com>
2018-05-17 11:46:23 +0800

    预加载

---
+++ src/main/java/com/puyixiaowo/fbook/bean/book/PickRulesBean.java
@@ -0,0 +1,29 @@
+package com.puyixiaowo.fbook.bean.book;
+
+import java.io.Serializable;
+
+public class PickRulesBean implements Serializable {
+    private static final long serialVersionUID = -6808379757080016944L;
+
+    private String id;
+
+    private String bookDetailTitle;
+    private String bookDetailAuthor;
+    private String bookDetailUpdateDate;
+    private String bookDetailUpdateChapter;
+    private String bookDetailCategory;
+    private String bookDetailFaceUrl;
+    private String bookDetailIntro;
+
+    private String chapterListTitle;
+    private String chapterListLink;
+
+    private String chapterDetailTitle;
+    private String chapterDetailContent;
+    private String chapterDetailMaxPage;
+    private String chapterDetailCurrentPage;
+
+
+
+
+}
