83b0ccd9c74b9ef1021b7be5d4731940c8b27a0d
ysdxz207 <ysdxz207@qq.com>
2018-05-19 11:56:47 +0800

    女频，替换aid

--- src/main/java/com/puyixiaowo/fbook/bean/error/ReadError.java
+++ src/main/java/com/puyixiaowo/fbook/bean/error/ReadError.java
@@ -2,8 +2,8 @@ package com.puyixiaowo.fbook.bean.error;
 
 public enum ReadError implements Error{
 
-    READ_LAST_CHAPTER_ERROR("已经是最后一章啦"),
-    READ_SOURCE_ERROR("貌似书源有问题");
+    READ_LAST_CHAPTER_ERROR("已经是最后一章啦！"),
+    READ_SOURCE_ERROR("貌似书源有问题，再试试看。");
 
 
     public String msg;
